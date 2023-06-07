6390ae63b388932cd24eb10979fb6576ec2863bf
zakirya592 <muhammadzakirya592@gmail.com>
2023-06-07 15:13:37 +0500

    updata

--- src/Component/AllRounter/Pagerounter.jsx
+++ src/Component/AllRounter/Pagerounter.jsx
@@ -37,6 +37,7 @@ function Pagerounter() {
 
           {/* Asset Management */}
           <Route exact path='/assetmanagement' element={<AssetManagement/>}/>
+          {/*My Add more pages here  and */}
         </Routes>
       </BrowserRouter>
     </>
