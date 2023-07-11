2ba4be8c43287a8a88f383450d6a53a21f9a5318
zakirya592 <muhammadzakirya592@gmail.com>
2023-07-11 13:41:20 +0500

    updata

---
+++ src/Component/AllRounter/setup configuration/Day/Newday.jsx
@@ -0,0 +1,169 @@
+import React, { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import Fade from '@mui/material/Fade';
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import axios from 'axios'
+import Swal from "sweetalert2";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+function Newday() {
+
+    const [value, setvalue] = useState({
+        DaysCode: '', DaysSeq: '', DaysDesc: ''
+    })
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+        window.location.reload(); // Reload the page
+    };
+
+    const postapi = (e) => {
+        e.preventDefault();
+        axios.post(`/api/Days_post`, {
+            DaysCode: value.DaysCode,
+            DaysSeq: value.DaysSeq,
+            DaysDesc: value.DaysDesc
+        },)
+            .then((res) => {
+                console.log('Add', res.data);
+                setvalue(prevState => ({ ...prevState, DaysCode: '', DaysSeq: '', DaysDesc: '' }));
+                // setAnchorEl(null);
+                toast.success('Add record successfully', {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                toast.error(`The Error is due to ${err}`, {
+                    position: "bottom-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
+            });
+    }
+
+
+    return (
+        <>
+            <button type="button" id="fade-button"
+                aria-controls={open ? 'fade-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick} className="btn btn-outline-primary mx-1 color2 btnwork">
+                <AddCircleOutlineIcon className="me-1" />
+                New
+            </button>
+
+            <Menu
+                id="fade-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'fade-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                // onClose={handleClose}
+                TransitionComponent={Fade}
+            >
+
+                <form onSubmit={postapi}>
+                    <div className="row mx-auto px-3 w-100 formsection ">
+                        <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                            <div className='emailsection position-relative d-grid my-1'>
+                                <label htmlFor='DaysCode' className='lablesection color3 text-start mb-1'>
+                                    Days Code<span className='star'>*</span>
+                                </label>
+
+                                <input
+                                    types='text'
+                                    id='DaysCode'
+                                    value={value.DaysCode}
+                                    onChange={e => {
+                                        setvalue(prevValue => ({
+                                            ...prevValue,
+                                            DaysCode: e.target.value
+                                        }))
+                                    }}
+                                    className='rounded inputsection py-2 px-2'
+                                    placeholder='Days Code'
+                                    required
+                                ></input>
+                            </div>
+                        </div>
+                        <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                            <div className='emailsection position-relative d-grid my-1'>
+                                <label htmlFor='DaysSeq' className='lablesection color3 text-start mb-1'>
+                                    Days Seq<span className='star'>*</span>
+                                </label>
+
+                                <input
+                                    types='text'
+                                    id='DaysSeq'
+                                    value={value.DaysSeq}
+                                    onChange={e => {
+                                        setvalue(prevValue => ({
+                                            ...prevValue,
+                                            DaysSeq: e.target.value
+                                        }))
+                                    }}
+                                    className='rounded inputsection py-2 px-2'
+                                    placeholder='Days Seq'
+                                    required
+                                ></input>
+                            </div>
+                        </div>
+                        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                            <div className='emailsection position-relative d-grid my-1'>
+                                <label htmlFor='DaysDesc' className='lablesection color3 text-start mb-1'>
+                                    Days Desc <span className='star'>*</span>
+                                </label>
+
+                                <input
+                                    types='text'
+                                    id='DaysDesc'
+                                    value={value.DaysDesc}
+                                    onChange={e => {
+                                        setvalue(prevValue => ({
+                                            ...prevValue,
+                                            DaysDesc: e.target.value
+                                        }))
+                                    }}
+                                    className='rounded inputsection py-2 px-2'
+                                    placeholder='Days Desc'
+                                    required
+                                ></input>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="d-flex justify-content-between my-2 p-4 ">
+                        <button type="button" class="border-0 px-3  savebtn py-2" onClick={handleClose}><ArrowCircleLeftOutlinedIcon className='me-2' />Back</button>
+                        <button type="submit" class="border-0 px-3  savebtn py-2" ><AddCircleOutlineIcon className='me-2' />Add New</button>
+                    </div>
+
+                </form>
+
+            </Menu>
+            <ToastContainer />
+        </>
+    )
+}
+
+export default Newday
