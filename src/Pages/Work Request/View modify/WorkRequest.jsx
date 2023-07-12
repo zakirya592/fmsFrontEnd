5a5f31b20858dfeb886f398454b115725eb28624
zakirya592 <muhammadzakirya592@gmail.com>
2023-07-12 13:18:31 +0500

    updata

--- src/Pages/Work Request/View modify/WorkRequest.jsx
+++ src/Pages/Work Request/View modify/WorkRequest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
 import Siderbar from '../../../Component/Siderbar/Siderbar';
 import AppBar from '@mui/material/AppBar';
@@ -18,16 +18,31 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import axios from 'axios';
 function WorkRequest() {
-    const navigate = useNavigate();   
-
+  const navigate = useNavigate();
+  const [getdata, setgetdata] = useState([])
+  const getapi = () => {
+    axios.get(`/api/workRequest_GET_LIST`, {
+    },)
+      .then((res) => {
+        console.log('TO get the list', res);
+        setgetdata(res.data.recordset)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
+    getapi()
+  }, [])
   const columns = [
-    { field: 'SEQ.', headerName: 'SEQ.', width: 90 },
-    { field: 'WORK REQUEST#', headerName: 'WORK REQUEST#', width: 160 },
-    { field: 'REQUEST STATUS', headerName: 'REQUEST STATUS', width: 160 },
-    { field: 'REQUEST BY EMP#', headerName: 'REQUEST BY EMP#', width: 160 },
-    { field: 'PRIORITY', headerName: 'PRIORITY', width: 150 },
-    { field: 'REQUEST DATE', headerName: 'REQUEST DATE', width: 150 },
+    { field: 'id', headerName: 'SEQ.', width: 90 },
+    { field: 'RequestNumber', headerName: 'WORK REQUEST#', width: 160 },
+    { field: 'RequestStatus', headerName: 'REQUEST STATUS', width: 160 },
+    { field: 'EmployeeID', headerName: 'REQUEST BY EMP#', width: 160 },
+    { field: 'WorkPriority', headerName: 'PRIORITY', width: 150 },
+    { field: 'RequestDateTime', headerName: 'REQUEST DATE', width: 200 },
     { field: 'WORK TYPE DESC', headerName: 'WORK TYPE DESC', width: 160 },
     { field: 'WORK TRADE DESC', headerName: 'WORK TRADE DESC', width: 160 },
     { field: 'ACTIONS', headerName: 'ACTIONS', width: 140, renderCell: ActionButtons },
@@ -53,66 +68,91 @@ function WorkRequest() {
       // Handle delete action
       handleMenuClose();
     };
-    
+
     return (
-<div>
-  <Button className='actionBtn' onClick={handleMenuOpen} style={{color: "black"}}>
-    <span style={{paddingRight: '10px'}}>Action</span>
-    <ArrowDropDownIcon />
-  </Button>
-  <Menu
-    anchorEl={anchorEl}
-    open={Boolean(anchorEl)}
-    onClose={handleMenuClose}
-  >
-    <MenuItem>
-      <span style={{paddingRight: '18px'}}>View</span>
-      <VisibilityIcon />
-    </MenuItem>
-    <MenuItem onClick={handleUpdate}>
-      <span style={{paddingRight: '3px'}}>Update</span>
-      <EditIcon />
-    </MenuItem>
-    <MenuItem onClick={handleDeleteButtonClick}>
-      <span style={{paddingRight: '10px'}}>Delete</span>
-      <DeleteIcon />
-    </MenuItem>
-  </Menu>
-</div>
+      <div>
+        <Button className='actionBtn' onClick={handleMenuOpen} style={{ color: "black" }}>
+          <span style={{ paddingRight: '10px' }}>Action</span>
+          <ArrowDropDownIcon />
+        </Button>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+        >
+          <MenuItem>
+            <span style={{ paddingRight: '18px' }}>View</span>
+            <VisibilityIcon />
+          </MenuItem>
+          <MenuItem onClick={handleUpdate}>
+            <span style={{ paddingRight: '3px' }}>Update</span>
+            <EditIcon />
+          </MenuItem>
+          <MenuItem onClick={handleDeleteButtonClick}>
+            <span style={{ paddingRight: '10px' }}>Delete</span>
+            <DeleteIcon />
+          </MenuItem>
+        </Menu>
+      </div>
 
 
-      );
-    }
+    );
+  }
 
-  const generateRandomData = () => {
-    const rows = [];
-    for (let i = 1; i <= 10; i++) {
-      rows.push({
-        id: i,
-        'SEQ.': i,
-        'WORK REQUEST#': `Work Request-${i}`,
-        'REQUEST STATUS': Math.random() < 0.5 ? 'Pending' : 'Completed',
-        'REQUEST BY EMP#': `EMPNO-${i}`,
-        'PRIORITY': Math.random()<0.5 ? "High" :"Low",
-        'REQUEST DATE': new Date().toLocaleDateString(),
-        'WORK TYPE DESC': `work type desc-${i}`,
-        'WORK TRADE DESC': `work type desc-${i}`,
-        'ACTION': `Building ${i}`,
-      });
-    }
-    return rows;
-  };
+  // const generateRandomData = () => {
+  //   const rows = [];
+  //   for (let i = 1; i <= 10; i++) {
+  //     rows.push({
+  //       id: i,
+  //       'SEQ.': i,
+  //       'WORK REQUEST#': `Work Request-${i}`,
+  //       'REQUEST STATUS': Math.random() < 0.5 ? 'Pending' : 'Completed',
+  //       'REQUEST BY EMP#': `EMPNO-${i}`,
+  //       'PRIORITY': Math.random() < 0.5 ? "High" : "Low",
+  //       'REQUEST DATE': new Date().toLocaleDateString(),
+  //       'WORK TYPE DESC': `work type desc-${i}`,
+  //       'WORK TRADE DESC': `work type desc-${i}`,
+  //       'ACTION': `Building ${i}`,
+  //     });
+  //   }
+  //   return rows;
+  // };
 
-  const rows = generateRandomData();
+  // const rows = generateRandomData();
 
   const [requestByEmployee, setrequestByEmployee] = useState('');
   const [RequestStatusFilterValue, setRequestStatusFilterValue] = useState('');
 
-  const filteredRows = rows.filter((row) =>
-    row['REQUEST BY EMP#'].toLowerCase().includes(requestByEmployee.toLowerCase()) &&
-    row['REQUEST STATUS'].toLowerCase().includes(RequestStatusFilterValue.toLowerCase())
-  );
+  // const filteredRows = rows.filter((row) =>
+  //   row['REQUEST BY EMP#'].toLowerCase().includes(requestByEmployee.toLowerCase()) &&
+  //   row['REQUEST STATUS'].toLowerCase().includes(RequestStatusFilterValue.toLowerCase())
+  // );
 
+  const filteredRows = getdata && getdata.map((row, indes) => ({
+    ...row,
+    id: indes + 1,
+    RequestNumber: row.RequestNumber,
+    RequestStatus: row.RequestStatus ,
+    EmployeeID: row.EmployeeID,
+    WorkPriority: row.WorkPriority,
+    RequestDateTime:row.RequestDateTime,
+    WorkType: row.WorkType,
+  }))
+
+  useEffect(() => {
+    axios.get(`/api/WorkType_descri_LIST/${filteredRows}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [])
+  
+  const [paginationModel, setPaginationModel] = React.useState({
+    pageSize: 25,
+    page: 0,
+  });
   return (
     <>
       <div className="bg">
@@ -133,11 +173,11 @@ function WorkRequest() {
                 <div className="py-3">
                   <div className="d-flex justify-content-between my-auto">
                     <p className="color1 workitoppro my-auto">
-                        Work Request Transactions<span className='star'>*</span></p>
+                      Work Request Transactions<span className='star'>*</span></p>
                     <div className="d-flex">
-                    <button type="button" className="btn btn-outline-primary mx-1 color2 btnwork" onClick={(() => {
-                                            navigate('/createworkrequest')
-                                        })}><AddCircleOutlineIcon className='me-1' />Create</button>
+                      <button type="button" className="btn btn-outline-primary mx-1 color2 btnwork" onClick={(() => {
+                        navigate('/createworkrequest')
+                      })}><AddCircleOutlineIcon className='me-1' />Create</button>
                       <button type="button" className="btn btn-outline-primary mx-1 color2 btnwork">
                         <PrintIcon className="me-1" />
                         Print
@@ -149,79 +189,85 @@ function WorkRequest() {
                   </div>
                   <hr className="color3 line" />
                   {/* Search Fields */}
-                  <div className="row mx-auto formsection"> 
-                  <div className="col-sm-10 col-md-4 col-lg-4 col-xl-3 ">
-                                    <div className='emailsection position-relative d-grid my-2'>
-                                        <label  className='lablesection color3 text-start mb-1 filter-label'>
-                                        Employee Number<span className='star'>*</span>                                        </label>
+                  <div className="row mx-auto formsection">
+                    <div className="col-sm-10 col-md-4 col-lg-4 col-xl-3 ">
+                      <div className='emailsection position-relative d-grid my-2'>
+                        <label className='lablesection color3 text-start mb-1 filter-label'>
+                          Employee Number<span className='star'>*</span>                                        </label>
 
-                                        <input
-                                            types='text'
-                                            id='Employeenumber'
-                                            placeholder="Select Employee # "
-                                            value={requestByEmployee}
-                                            className='rounded inputsection py-2'
-                                            onChange={(e) => setrequestByEmployee(e.target.value)}
-                                        ></input>
-                                        <p
-                                            className='position-absolute text-end serachicon'
-                                        >
-                                            <SearchOutlined className=' serachicon' />
-                                        </p>
-                                    </div>
+                        <input
+                          types='text'
+                          id='Employeenumber'
+                          placeholder="Select Employee # "
+                          value={requestByEmployee}
+                          className='rounded inputsection py-2'
+                          onChange={(e) => setrequestByEmployee(e.target.value)}
+                        ></input>
+                        <p
+                          className='position-absolute text-end serachicon'
+                        >
+                          <SearchOutlined className=' serachicon' />
+                        </p>
+                      </div>
                     </div>
-                  <div className="col-sm-10 col-md-5 col-lg-5 col-xl-4 ">
-                                    <div className='emailsection position-relative d-grid my-2'>
-                                        <label  className='lablesection color3 text-start mb-1 filter-label'>
-                                        Employee Name<span className='star'>*</span>                                        </label>
+                    <div className="col-sm-10 col-md-5 col-lg-5 col-xl-4 ">
+                      <div className='emailsection position-relative d-grid my-2'>
+                        <label className='lablesection color3 text-start mb-1 filter-label'>
+                          Employee Name<span className='star'>*</span>                                        </label>
 
-                                        <input
-                                            types='text'
-                                            id='Employeenumber'
-                                            placeholder="Firstname Middlename Lastname"
-                                            // value={locationFilterValue}
-                                            className='rounded inputsection py-2'
-                                        ></input>
-                                        <p
-                                            className='position-absolute text-end serachicon'
-                                        >
-                                            <SearchOutlined className=' serachicon' />
-                                        </p>
-                                    </div>
+                        <input
+                          types='text'
+                          id='Employeenumber'
+                          placeholder="Firstname Middlename Lastname"
+                          // value={locationFilterValue}
+                          className='rounded inputsection py-2'
+                        ></input>
+                        <p
+                          className='position-absolute text-end serachicon'
+                        >
+                          <SearchOutlined className=' serachicon' />
+                        </p>
+                      </div>
                     </div>
                     <div className="col-sm-10 col-md-4 col-lg-4 col-xl-3">
-  <div className='emailsection position-relative d-grid my-2'>
-    <label className='lablesection color3 text-start mb-1 filter-label'>
-      Request Status<span className='star'>*</span>
-    </label>
+                      <div className='emailsection position-relative d-grid my-2'>
+                        <label className='lablesection color3 text-start mb-1 filter-label'>
+                          Request Status<span className='star'>*</span>
+                        </label>
 
-    <select
-      id='RequestStatus'
-      value={RequestStatusFilterValue}
-      className='rounded inputsection py-2'
-      onChange={(e) => setRequestStatusFilterValue(e.target.value)}
-    >
-      <option value=''>Select Status</option>
-      <option value='completed'>Completed</option>
-      <option value='pending'>Pending</option>
-    </select>
+                        <select
+                          id='RequestStatus'
+                          value={RequestStatusFilterValue}
+                          className='rounded inputsection py-2'
+                          onChange={(e) => setRequestStatusFilterValue(e.target.value)}
+                        >
+                          <option value=''>Select Status</option>
+                          <option value='Open'>Open</option>
+                          <option value='Closed'>Closed</option>
+                          <option value='Cancelled'>Cancelled</option>
+                        </select>
 
-  </div>
-</div>
-
+                      </div>
                     </div>
+
+                  </div>
                   <div style={{ height: 420, width: '100%' }}>
                     <DataGrid
                       rows={filteredRows}
                       columns={columns}
-                      pageSize={5}
+                      pagination
+                      rowsPerPageOptions={[10, 25, 500]} // Optional: Set available page size options
+                      paginationModel={paginationModel}
+                      onPaginationModelChange={setPaginationModel}
                       checkboxSelection
                       disableRowSelectionOnClick
+                      disableMultipleSelection
                     />
+
                   </div>
                   <div className="d-flex justify-content-between mt-3">
-                                <button type="button" class="border-0 px-3  savebtn py-2"><ArrowCircleLeftOutlinedIcon className='me-2' />Back</button>
-                            </div>
+                    <button type="button" class="border-0 px-3  savebtn py-2"><ArrowCircleLeftOutlinedIcon className='me-2' />Back</button>
+                  </div>
                 </div>
               </div>
             </Box>
