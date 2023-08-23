b046065096e360f05bccb3054856e7ce6f94de38
zakirya592 <muhammadzakirya592@gmail.com>
2023-08-23 09:53:07 +0500

    updata

--- src/Pages/Work Request/View modify/WorkRequest.jsx
+++ src/Pages/Work Request/View modify/WorkRequest.jsx
@@ -447,7 +447,7 @@ function WorkRequest() {
                 <div className="py-3">
                   <div className="d-flex justify-content-between my-auto">
                     <p className="color1 workitoppro my-auto">
-                      Work Request Transactions<span className='star'>*</span></p>
+                      Work Request Transactions
                     <div className="d-flex">
                       <button type="button" className="border-0 px-3  savebtn py-2" disabled={statuscheck === 'Closed' || selectedRowIds.length === 0}  onClick={handleAddToWorkRequest}>UPDATE</button>
 
