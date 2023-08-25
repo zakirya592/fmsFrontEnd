a160a7d0133766557bd552cedad8f7a3647574cf
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2023-08-25 23:23:49 +0500

    change the modal

--- src/Component/AllRounter/setup configuration/Work types/Createwroke.jsx
+++ src/Component/AllRounter/setup configuration/Work types/Createwroke.jsx
@@ -1,106 +1,98 @@
 import React, { useState } from 'react';
-import Menu from '@mui/material/Menu';
-import Fade from '@mui/material/Fade';
 import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import axios from 'axios'
+import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-function Createwroke() {
-    // generateId random
+import Dialog from '@mui/material/Dialog';
+import "./CreateWork.css"
+
+function CreateWork() {
     const generateId = () => {
         const randomNumber = Math.floor(Math.random() * 100000000);
         return randomNumber.toString().padStart(8, '0');
     };
-    const [value, setvalue] = useState({
-        EmployeeID: '', WorkTypeCode: '', WorkTypeDesc: ''
-    })
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+
+    const [value, setValue] = useState({
+        EmployeeID: '',
+        WorkTypeCode: '',
+        WorkTypeDesc: '',
+    });
+
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleOpenDialog = () => {
+        setOpenDialog(true);
     };
-    const handleClose = () => {
-        setAnchorEl(null);
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
         window.location.reload(); // Reload the page
     };
 
     const postapi = (e) => {
         e.preventDefault();
         const generatedId = generateId();
-        axios.post(`/api/WorkType_post`, {
-            WorkTypeCode: value.WorkTypeCode,
-            WorkTypeDesc: value.WorkTypeDesc,
-        },)
+        axios
+            .post(`/api/WorkType_post`, {
+                WorkTypeCode: value.WorkTypeCode,
+                WorkTypeDesc: value.WorkTypeDesc,
+            })
             .then((res) => {
                 console.log('Add', res.data);
-                setvalue(prevState => ({ ...prevState, WorkTypeCode: '', WorkTypeDesc: '' }));
-                // setAnchorEl(null);
+                setValue({ WorkTypeCode: '', WorkTypeDesc: '' });
                 toast.success('Add record successfully', {
-                    position: "bottom-center",
+                    position: 'bottom-center',
                     autoClose: 5000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
                     progress: undefined,
-                    theme: "light",
+                    theme: 'light',
                 });
+                handleCloseDialog(); // Close the dialog after success
             })
             .catch((err) => {
                 console.log(err);
-                toast.error(`The Record already exists in the table`, {
-                    position: "bottom-center",
+                toast.error('The Record already exists in the table', {
+                    position: 'bottom-center',
                     autoClose: 5000,
                     hideProgressBar: false,
                     closeOnClick: true,
                     pauseOnHover: true,
                     draggable: true,
                     progress: undefined,
-                    theme: "light",
+                    theme: 'light',
                 });
             });
-    }
-
+    };
 
     return (
         <>
-            <button type="button" id="fade-button"
-                aria-controls={open ? 'fade-menu' : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? 'true' : undefined}
-                onClick={handleClick} className="btn btn-outline-primary mx-1 color2 btnwork">
+            <button
+                type="button"
+                onClick={handleOpenDialog}
+                className="btn btn-outline-primary mx-1 color2 btnwork"
+            >
                 <AddCircleOutlineIcon className="me-1" />
                 New
             </button>
 
-            <Menu
-                id="fade-menu"
-                MenuListProps={{
-                    'aria-labelledby': 'fade-button',
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                // onClose={handleClose}
-                TransitionComponent={Fade}
-
-            >
-
-                <form onSubmit={postapi}>
-
-                    <div className="row mx-auto w-100 formsection firstname1">
+            <Dialog open={openDialog} onClose={handleCloseDialog} className='DialogSizing'style={{ top: '-300px', left: '650px' }}>
+                <form onSubmit={postapi} className='Sizing'>
+                    <div className="row mx-auto w-100 formsection sizingtop">
                         <div className="col-sm-12 col-md-5 col-lg-5 col-xl-5">
                             <div className='emailsection position-relative d-grid my-1'>
                                 <label htmlFor='WorkTypeCode' className='lablesection color3 text-start mb-1'>
                                     WorkType Code<span className='star'>*</span>
                                 </label>
-
                                 <input
-                                    types='text'
+                                    type='text'
                                     id='WorkTypeCode'
                                     value={value.WorkTypeCode}
                                     onChange={e => {
-                                        setvalue(prevValue => ({
+                                        setValue(prevValue => ({
                                             ...prevValue,
                                             WorkTypeCode: e.target.value
                                         }))
@@ -108,7 +100,7 @@ function Createwroke() {
                                     className='rounded inputsection py-2 px-2'
                                     placeholder='WorkType Code'
                                     required
-                                ></input>
+                                />
                             </div>
                         </div>
                         <div className="col-sm-12 col-md-7 col-lg-7 col-xl-7">
@@ -116,13 +108,12 @@ function Createwroke() {
                                 <label htmlFor='WorkTypeDesc' className='lablesection color3 text-start mb-1'>
                                     WorkType Desc<span className='star'>*</span>
                                 </label>
-
                                 <input
-                                    types='text'
+                                    type='text'
                                     id='WorkTypeDesc'
                                     value={value.WorkTypeDesc}
                                     onChange={e => {
-                                        setvalue(prevValue => ({
+                                        setValue(prevValue => ({
                                             ...prevValue,
                                             WorkTypeDesc: e.target.value
                                         }))
@@ -130,24 +121,32 @@ function Createwroke() {
                                     className='rounded inputsection py-2 px-2'
                                     placeholder='WorkType Desc'
                                     required
-                                ></input>
+                                />
                             </div>
                         </div>
-
                     </div>
 
                     <div className="d-flex justify-content-between my-2 p-4 ">
-                        <button type="button" class="border-0 px-3  savebtn py-2" onClick={handleClose}><ArrowCircleLeftOutlinedIcon className='me-2' />Back</button>
-                        <button type="submit" class="border-0 px-3  savebtn py-2" ><AddCircleOutlineIcon className='me-2' />Add New</button>
+                        <button
+                            type="button"
+                            className="border-0 px-3 savebtn py-2"
+                            onClick={handleCloseDialog}
+                        >
+                            <ArrowCircleLeftOutlinedIcon className="me-2" /> Back
+                        </button>
+                        <button
+                            type="submit"
+                            className="border-0 px-3 savebtn py-2"
+                        >
+                            <AddCircleOutlineIcon className="me-2" /> Add New
+                        </button>
                     </div>
-
                 </form>
+            </Dialog>
 
-            </Menu>
             <ToastContainer />
-
         </>
-    )
+    );
 }
 
-export default Createwroke
+export default CreateWork;
