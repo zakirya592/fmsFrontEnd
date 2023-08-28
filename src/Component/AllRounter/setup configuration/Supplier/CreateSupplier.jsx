41f1cce832172cf8fa718e2c08e6cc79bd29c4a6
Abdul Rehman <97659086+abdul191@users.noreply.github.com>
2023-08-29 00:36:59 +0500

    Post api added to the create page

--- src/Component/AllRounter/setup configuration/Supplier/CreateSupplier.jsx
+++ src/Component/AllRounter/setup configuration/Supplier/CreateSupplier.jsx
@@ -38,8 +38,8 @@ function CreateSupplier() {
         }));
       };
     
-      const handleSave = () => {
-        const dataToSend = {
+      const addSupplier = async () => {
+        axios.post(`/api/VendorMaster_post`, {
           VendorID: value.VendorID,
           VendorName: value.VendorName,
           VendorAddress: value.VendorAddress,
@@ -50,39 +50,45 @@ function CreateSupplier() {
           ContactLandlineNumber: value.ContactLandlineNumber,
           ContactEmail: value.ContactEmail,
           VendorInformation: value.VendorInformation,
-        };
-    
-        axios
-          .post(`/api/VendorMaster_post`, dataToSend)
-          .then((res) => {
-            console.log(res.data);
-            Swal.fire('Created!', `Vendor Master ${value.VendorID} has been created successfully`, 'success');
-            setValue({
-              VendorID: '',
-              VendorName: '',
-              VendorAddress: '',
-              ContactLastname: '',
-              ContactFirstname: '',
-              ContactMiddlename: '',
-              ContactMobileNumber: '',
-              ContactLandlineNumber: '',
-              ContactEmail: '',
-              VendorInformation: '',
+        },)
+            .then((res) => {
+                console.log(res.data);
+                Swal.fire(
+                    'Created!',
+                    `Supplier ${value.VendorID} has been created successfully`,
+                    'success'
+                )
+                navigate('/supplier')
+
+            })
+            .catch((err) => {
+                console.log(err);
+                const statuss = err.response.status
+
+                if (statuss == 404) {
+                    Swal.fire(
+                        'Error!',
+                        'Supplier is required  ',
+                        'error'
+                    )
+                }
+                else if (statuss == 500) {
+                    Swal.fire(
+                        'Error!',
+                        `This Supplier Already exit`,
+                        'error'
+                    )
+                }
+                // else {
+                //     Swal.fire(
+                //         'Error!',
+                //         `${err.response.message}`,
+                //         'error'
+                //     )
+                // }
             });
-          })
-          .catch((err) => {
-            console.log(err);
-            const statuss = err.response.status;
-    
-            if (statuss === 404) {
-              Swal.fire('Error!', 'VendorID is required', 'error');
-            } else if (statuss === 500) {
-              Swal.fire('Error!', 'This VendorID Already exists', 'error');
-            } else {
-              Swal.fire('Error!', `${err.response.message}`, 'error');
-            }
-          });
-      };
+
+    };
     return (
         <>
         <div className="bg">
@@ -127,14 +133,14 @@ function CreateSupplier() {
                                         Vendor/Supplier Code<span className='star'>*</span>
                                     </label>
                                     <input
-                type="text"
-                id="SupplierCode"
-                value={value.SupplierCode} 
-                onChange={(e) => handleChange("SupplierCode", e.target.value)} 
-                className="rounded inputsection py-2"
-                placeholder="Enter Supplier Code"
-                required
-              ></input>
+    type="text"
+    id="VendorID"  
+    value={value.VendorID} 
+    onChange={(e) => handleChange("VendorID", e.target.value)}  
+    className="rounded inputsection py-2"
+    placeholder="Enter Supplier Code"
+    required
+/>
                                     <p
                                         className='position-absolute text-end serachicon'
                                     >
@@ -150,9 +156,9 @@ function CreateSupplier() {
     
                                     <input
       type="text"
-      id="SupplierName"
-      value={value.SupplierName} 
-      onChange={(e) => handleChange("SupplierName", e.target.value)} 
+      id="VendorName"
+      value={value.VendorName} 
+      onChange={(e) => handleChange("VendorName", e.target.value)} 
       className="rounded inputsection py-2"
       placeholder="Enter Vendor/Supplier Name"
       required
@@ -170,13 +176,14 @@ function CreateSupplier() {
                 Complete Address<span className="star">*</span>
             </label>
             <div className="form-floating inputsectiondropdpwn">
-                <textarea
-                    id="CompleteAddress"
-                    value={value.CompleteAddress} // Update this line
-                    onChange={(e) => handleChange("CompleteAddress", e.target.value)} // Update this line
-                    className="rounded inputsectiondropdpwn w-100 color2 py-2"
-                    placeholder="Enter Address Details"
-                ></textarea>
+            <textarea
+    id="VendorAddress"  // Update this to "VendorAddress"
+    value={value.VendorAddress}  // Update this to "VendorAddress"
+    onChange={(e) => handleChange("VendorAddress", e.target.value)}  // Update this to "VendorAddress"
+    className="rounded inputsectiondropdpwn w-100 color2 py-2"
+    placeholder="Enter Vendor Address"
+></textarea>
+
             </div>
         </div>
     </div>
@@ -192,72 +199,69 @@ function CreateSupplier() {
                         </Toolbar>
                         {/* below section */}
                         <div className="row mx-auto formsection">
-                        <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+    <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
         <div className="emailsection position-relative d-grid my-2">
-            <label htmlFor="FirstName" className="lablesection color3 text-start mb-1">
+            <label htmlFor="ContactFirstname" className="lablesection color3 text-start mb-1">
                 First Name<span className="star">*</span>
             </label>
             <input
                 type="text"
-                id="FirstName"
-                value={value.FirstName} // Update this line
-                onChange={(e) => handleChange("FirstName", e.target.value)} // Update this line
+                id="ContactFirstname"
+                value={value.ContactFirstname}
+                onChange={(e) => handleChange("ContactFirstname", e.target.value)}
                 className="rounded inputsection py-2"
                 placeholder="Enter Your First Name"
-                required
             />
             <p className="position-absolute text-end serachicon"></p>
         </div>
     </div>
     <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
         <div className="emailsection position-relative d-grid my-2">
-            <label htmlFor="MiddleName" className="lablesection color3 text-start mb-1">
+            <label htmlFor="ContactMiddlename" className="lablesection color3 text-start mb-1">
                 Middle Name<span className="star">*</span>
             </label>
             <input
                 type="text"
-                id="MiddleName"
-                value={value.MiddleName} // Update this line
-                onChange={(e) => handleChange("MiddleName", e.target.value)} // Update this line
+                id="ContactMiddlename"
+                value={value.ContactMiddlename}
+                onChange={(e) => handleChange("ContactMiddlename", e.target.value)}
                 className="rounded inputsection py-2"
                 placeholder="Enter Your Middle Name"
-                required
             />
             <p className="position-absolute text-end serachicon"></p>
         </div>
     </div>
     <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
         <div className="emailsection position-relative d-grid my-2">
-            <label htmlFor="LastName" className="lablesection color3 text-start mb-1">
+            <label htmlFor="ContactLastname" className="lablesection color3 text-start mb-1">
                 Last Name<span className="star">*</span>
             </label>
             <input
                 type="text"
-                id="LastName"
-                value={value.LastName} // Update this line
-                onChange={(e) => handleChange("LastName", e.target.value)} // Update this line
+                id="ContactLastname"
+                value={value.ContactLastname}
+                onChange={(e) => handleChange("ContactLastname", e.target.value)}
                 className="rounded inputsection py-2"
                 placeholder="Enter Your Last Name"
-                required
             />
             <p className="position-absolute text-end serachicon"></p>
         </div>
     </div>
-    
-                            </div>
+</div>
+
                             
                             {/* mobile number */}
-                           < div className="row mx-auto">
-                           <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                            <div className="row mx-auto">
+    <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
         <div className="emailsection d-grid my-2">
-            <label htmlFor="MobileNumber" className="lablesection color3 text-start mb-1">
+            <label htmlFor="ContactMobileNumber" className="lablesection color3 text-start mb-1">
                 Mobile Number
             </label>
             <PhoneInput
-                placeholder="+966   500000000"
-                id="MobileNumber"
-                value={value.mobileNumber} // Update this line
-                onChange={(phoneNumber) => handleChange("mobileNumber", phoneNumber)} // Update this line
+                placeholder="+966 500000000"
+                id="ContactMobileNumber"
+                value={value.ContactMobileNumber} // Update this line
+                onChange={(phoneNumber) => handleChange("ContactMobileNumber", phoneNumber)} // Update this line
                 className="customstyling rounded inputsection custom-phone-input customstyling"
                 defaultCountry="SA"
                 dropdownClass="custom-phone-dropdown"
@@ -266,66 +270,64 @@ function CreateSupplier() {
     </div>
     <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
         <div className="emailsection d-grid my-2">
-            <label htmlFor="LandlineNumber" className="lablesection color3 text-start mb-1">
+            <label htmlFor="ContactLandlineNumber" className="lablesection color3 text-start mb-1">
                 Landline Number
             </label>
             <PhoneInput
-                placeholder="+966   500000000"
-                id="LandlineNumber"
-                value={value.LandlineNumber} // Update this line
-                onChange={(phoneNumber) => handleChange("LandlineNumber", phoneNumber)} // Update this line
+                placeholder="+966 500000000"
+                id="ContactLandlineNumber"
+                value={value.ContactLandlineNumber} // Update this line
+                onChange={(phoneNumber) => handleChange("ContactLandlineNumber", phoneNumber)} // Update this line
                 className="customstyling rounded inputsection custom-phone-input customstyling"
                 defaultCountry="SA"
                 dropdownClass="custom-phone-dropdown"
             />
         </div>
     </div>
-    
+
     {/* email here */}
     <div className="col-sm-7 col-md-3 col-lg-3 col-xl-3">
         <div className="emailsection position-relative d-grid my-2">
-            <label htmlFor="EmailAddress" className="lablesection color3 text-start mb-1">
+            <label htmlFor="ContactEmail" className="lablesection color3 text-start mb-1">
                 Email Address
             </label>
             <input
                 type="email"
-                id="EmailAddress"
-                value={value.email} // Update this line
-                onChange={(e) => handleChange("email", e.target.value)} // Update this line
+                id="ContactEmail"
+                value={value.ContactEmail} // Update this line
+                onChange={(e) => handleChange("ContactEmail", e.target.value)} // Update this line
                 className="rounded inputsection py-2"
                 placeholder="Enter your Email Address"
-                required
             />
         </div>
     </div>
-    
-    
+
     {/* vonderInformation */}
     <div className="col-sm-12 col-md-9 col-lg-9 col-xl-9 ">
         <div className='emailsection d-grid my-2'>
-            <label htmlFor='VonderInformation' className='lablesection color3 text-start mb-1'>
-                Vonder Information<span className='star'>*</span>
+            <label htmlFor='VendorInformation' className='lablesection color3 text-start mb-1'>
+                Vendor Information<span className='star'>*</span>
             </label>
             <div className="form-floating inputsectiondropdpwn">
                 <textarea
                     className='rounded inputsectiondropdpwn w-100 color2 py-2'
                     placeholder="Enter Vendor related products or any additional information "
-                    id="VonderInformation"
-                    value={value.VonderInformation} // Update this line
-                    onChange={(e) => handleChange("VonderInformation", e.target.value)} // Update this line
+                    id="VendorInformation"
+                    value={value.VendorInformation} // Update this line
+                    onChange={(e) => handleChange("VendorInformation", e.target.value)} // Update this line
                 ></textarea>
             </div>
         </div>
     </div>
-    
-                        </div>
+</div>
+
                     </div>
                     <div className="d-flex justify-content-between w-100 mt-3 mb-3">
                         <button type="button" className="border-0 px-3 savebtn py-2" onClick={() => { navigate(`/`); }}>
                             <ArrowCircleLeftOutlinedIcon className='me-2' />
                             Back
                         </button>
-                        <button type="button" className="border-0 px-3 savebtn py-2" onClick={handleSave}>
+                        <button type="button" className="border-0 px-3 savebtn py-2" onClick={addSupplier}>
                             <SaveAsIcon className='me-2' />
                             Save
                         </button>
