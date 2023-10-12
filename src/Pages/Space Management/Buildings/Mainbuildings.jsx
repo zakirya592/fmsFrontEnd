19ba772e5dacd3b8b2e9507a8f2e18d80fb36f98
zakirya592 <muhammadzakirya592@gmail.com>
2023-10-12 13:25:20 +0500

    updata

--- src/Pages/Space Management/Buildings/Mainbuildings.jsx
+++ src/Pages/Space Management/Buildings/Mainbuildings.jsx
@@ -179,6 +179,55 @@ function Mainbuildings() {
         // Example: sendToWorkRequest(selectedRowData);
     };
 
+    const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            console.log(file.type);
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const data = new Uint8Array(e.target.result);
+                const workbook = XLSX.read(data, { type: 'array' });
+                const sheetName = workbook.SheetNames[0]; // Assuming you have data in the first sheet
+                const sheet = workbook.Sheets[sheetName];
+                const json = XLSX.utils.sheet_to_json(sheet);
+                const formData = new FormData();
+
+                json.forEach((item) => {
+                    console.log(item.BuildingCode);
+                    formData.append('BuildingCode', item.BuildingCode.toString());
+                    formData.append('BuildingDesc', item.BuildingDesc);
+                    formData.append('Capacity', item.Capacity);
+                    formData.append('Latitude', item.Latitude);
+                    formData.append('Longtitude', item.Longtitude);
+                    formData.append('LocationCode', item.LocationCode);
+                    formData.append('BuildingImage', item.BuildingImage);
+
+                    axios.post(`/api/Building_newpage_post`, formData)
+                        .then((res) => {
+                            console.log('Add', res.data);
+                            // Handle success
+                            Swal.fire(
+                                'Add!',
+                                `Building has been created`,
+                                'success'
+                            )
+                            getapi()
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            Swal.fire(
+                                'Error!',
+                                `${err.response.data.error}`,
+                                'error'
+                            )
+                            // Handle errors
+                        });
+                });
+            };
+            reader.readAsArrayBuffer(file);
+
+        }
+    };
 
     const [paginationModel, setPaginationModel] = React.useState({
         pageSize: 25,
@@ -214,6 +263,12 @@ function Mainbuildings() {
                                             <button type="button" className="btn btn-outline-primary mx-1 color2 btnwork" onClick={(() => {
                                                 navigate('/Create/Buildings')
                                             })}><AddCircleOutlineIcon className='me-1' />Create</button>
+                                            <label type="button" className="btn btn-outline-primary mx-1 color2 btnwork" htmlFor="Importdata">
+                                                <img src={excel} alt="export" className='me-1' />
+                                                Import <GetAppIcon />
+                                            </label>
+                                            <input type="file" accept=".xlsx" onChange={handleFileUpload} className='d-none' id='Importdata' />
+
                                             <CSVLink data={getdata} type="button" className="btn btn-outline-primary color2" > <img src={excel} alt="export" className='me-1' htmlFor='epoet' /> Export  <FileUploadIcon />
                                             </CSVLink>
                                         </div>
